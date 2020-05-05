20b2f75ca40ba43a146f565b1da1c66eea6ea906
M0squ3ra <carlosmosquera1998@gmail.com>
2020-05-05 19:47:27 -0300

    codigo emprolijado

--- dndi.py
+++ dndi.py
@@ -1,37 +1,9 @@
 #!/usr/bin/env python3
-#organizar en binary tree por los hashes de los archivos, si coincide mandar a una lista la ruta del nuevo y la del anterior
-#trabajar con threads, uno para capturar cambios y otro para procesarlos
-#agregar una opcion verbose para ver cambios en carpetas ademas de si hay duplicados (-v)
 
-from btree import Btree
 import pyinotify
-import sys
 from event_handler import EventHandler
-import time
-from queue import Queue
 from util import *
 
-
-def initTree(path):
-	tree = Btree()
-	dirs = getDirs(path)
-	dirs = [path] + dirs
-	files = []
-
-	for j in dirs:
-		files = files + getFiles(j)
-
-	rep = []
-
-	for i in files:
-		state = tree.insert(getHash(i),i)
-		if state != None:
-			rep.append(state)
-
-	for i in rep:
-		printRep(i[1],i[3],i[0])
-
-	return tree,rep,dirs
 
 path = "/home/lol/Escritorio/do-not-duplicate-it/pruebas"
 
@@ -47,7 +19,6 @@ wdd = wm.add_watch(path, mask, rec=True)
 def process(notifier):
 	if(notifier.check_events()):
 		print("Nuevo evento")
-	return None #para que el loop no termine
+	return None
 
-notifier.loop(callback=None) #investigar, si retorna true, el loop termina
-# - Functor called after each event processing iteration
+notifier.loop(callback=None)
--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 from hashlib import md5
 from os import listdir
 from os.path import isdir,isfile
+from btree import Btree
 
 def printRep(parent,copy,hash):
 	print("[**] Duplicated file detected: \n" +
@@ -41,3 +42,24 @@ def getFiles(path):
 			files.append(path + "/" + i)
 
 	return files
+
+def initTree(path):
+	tree = Btree()
+	dirs = getDirs(path)
+	dirs = [path] + dirs
+	files = []
+
+	for j in dirs:
+		files = files + getFiles(j)
+
+	rep = []
+
+	for i in files:
+		state = tree.insert(getHash(i),i)
+		if state != None:
+			rep.append(state)
+
+	for i in rep:
+		printRep(i[1],i[3],i[0])
+
+	return tree,rep,dirs
